6b5711852f349cee6424b85b16f55cc1388c9cb1
Imane MOUTAOUAKEL <Imane.MOUTAOUAKEL@cpnv.ch>
2020-06-12 15:23:25 +0200

    Version Finale

--- Commands_has_products.cs
+++ Commands_has_products.cs
@@ -8,7 +8,10 @@ namespace Programme_pizzeria2
 {
     class Commands_has_products
     {
+        //mettre en private
         public String name;
+
+
         public int quantity;
         public double TotalProduct;
     }
